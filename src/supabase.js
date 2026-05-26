261a6f676dac2359964784b63424e5e1a5213c76
Magic Mookie <aldoniggi91@gmail.com>
2026-05-26 17:01:14 -0400

    Update Supabase anon key for security reasons

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://orupchhbprpwulovxovn.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ydXBjaHhicHJwd3Vsb3Z4b3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MTU3MzcsImV4cCI6MjA5NTM5MTczN30.0TCVm3FvGj9g5lUEoFdRs6HZw0OT5G-CcrsTDFSQlyI'
+const SUPABASE_ANON_KEY = 'sb_publishable_VveLKqEXrNGfA5kQGqzXCA_enIWHKLT'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
