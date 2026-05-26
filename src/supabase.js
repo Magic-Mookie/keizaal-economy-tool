7615138f71b72f8cb20a74165105b4f96f5ea512
Magic Mookie <aldoniggi91@gmail.com>
2026-05-26 17:15:53 -0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,16 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://orupchbprpwulovxovn.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ydXBjaHhicHJwd3Vsb3Z4b3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MTU3MzcsImV4cCI6MjA5NTM5MTczN30.0TCVm3FvGj9g5lUEoFdRs6HZw0OT5G-CcrsTDFSQlyI'
+const SUPABASE_URL = 'https://orupchxbprpwulovxovn.supabase.co'
+const SUPABASE_ANON_KEY = 'YOUR_LEGACY_ANON_KEY_HERE'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: false,
-    autoRefreshToken: false,
-  },
-  global: {
-    headers: {
-      'X-Client-Info': 'keizaal-economy-tool'
-    }
-  }
-})
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
