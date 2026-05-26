ae6be4aa153eaf239883ec24b08e87e67c6e409d
Magic Mookie <aldoniggi91@gmail.com>
2026-05-26 16:28:35 -0400

    Add Vite configuration for React project

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,27 @@
+## 3. vite.config.js
+```js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+})
+```
+
+---
+
+## 4. src/main.jsx
+```jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    
+  
+)
+```
+
+---
